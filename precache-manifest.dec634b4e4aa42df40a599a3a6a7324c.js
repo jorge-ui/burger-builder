55b85b658b947cae22da0e1fbc7210a5f5ac9a98
Jorge Rivera <jrdeveloper71@gmail.com>
2019-09-27 20:22:37 -0400

    Updates

---
+++ precache-manifest.dec634b4e4aa42df40a599a3a6a7324c.js
@@ -0,0 +1,154 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "428fd13b09cbcb365d3815a0b463b550",
+    "url": "/burger-builder/index.html"
+  },
+  {
+    "revision": "086b993cb38572c6dda1",
+    "url": "/burger-builder/static/css/3.b40cc5f1.chunk.css"
+  },
+  {
+    "revision": "a7ae45b244e3250235ad",
+    "url": "/burger-builder/static/css/4.31772804.chunk.css"
+  },
+  {
+    "revision": "99f97eee7d1877e54576",
+    "url": "/burger-builder/static/css/6.71ff8af1.chunk.css"
+  },
+  {
+    "revision": "86cf10a539f1d35c31fe",
+    "url": "/burger-builder/static/css/7.ea3c7515.chunk.css"
+  },
+  {
+    "revision": "b2cee7123fc3fb944e2c",
+    "url": "/burger-builder/static/css/8.eaaa40f7.chunk.css"
+  },
+  {
+    "revision": "b387cdf62424f82c804d",
+    "url": "/burger-builder/static/css/main.f46ca9c4.chunk.css"
+  },
+  {
+    "revision": "88771a93b256f6392bab",
+    "url": "/burger-builder/static/js/2.58718484.chunk.js"
+  },
+  {
+    "revision": "086b993cb38572c6dda1",
+    "url": "/burger-builder/static/js/3.824b5445.chunk.js"
+  },
+  {
+    "revision": "a7ae45b244e3250235ad",
+    "url": "/burger-builder/static/js/4.ea4fe28f.chunk.js"
+  },
+  {
+    "revision": "bad950296429aa92c3e8",
+    "url": "/burger-builder/static/js/5.1a4b15a9.chunk.js"
+  },
+  {
+    "revision": "99f97eee7d1877e54576",
+    "url": "/burger-builder/static/js/6.3b7b0028.chunk.js"
+  },
+  {
+    "revision": "86cf10a539f1d35c31fe",
+    "url": "/burger-builder/static/js/7.52ae361d.chunk.js"
+  },
+  {
+    "revision": "b2cee7123fc3fb944e2c",
+    "url": "/burger-builder/static/js/8.9ac55000.chunk.js"
+  },
+  {
+    "revision": "5a998297e689768bc015",
+    "url": "/burger-builder/static/js/9.cecacee8.chunk.js"
+  },
+  {
+    "revision": "b387cdf62424f82c804d",
+    "url": "/burger-builder/static/js/main.0df159b3.chunk.js"
+  },
+  {
+    "revision": "6a8b216e04ebbbaf79e7",
+    "url": "/burger-builder/static/js/runtime-main.c3158a47.js"
+  },
+  {
+    "revision": "e4ff23a8124a561f954f6cf0a78e5d11",
+    "url": "/burger-builder/static/media/bacon-icon.e4ff23a8.png"
+  },
+  {
+    "revision": "6352927674ca9fae0d0e667bc12461eb",
+    "url": "/burger-builder/static/media/bacon.63529276.png"
+  },
+  {
+    "revision": "04521d6167a219e83fd62d09b1a11701",
+    "url": "/burger-builder/static/media/bottomBread.04521d61.png"
+  },
+  {
+    "revision": "a24c6de6b1941649b7386e0387505ca8",
+    "url": "/burger-builder/static/media/egg-icon.a24c6de6.png"
+  },
+  {
+    "revision": "d9bdcbf950e00fa84688f7c6615f1a9d",
+    "url": "/burger-builder/static/media/jalapeno-icon.d9bdcbf9.png"
+  },
+  {
+    "revision": "edb4ad1c4d233e922ecf3ce9ecb34815",
+    "url": "/burger-builder/static/media/jalapeno.edb4ad1c.png"
+  },
+  {
+    "revision": "6133a8b72f638c1c3f40294137d4b51f",
+    "url": "/burger-builder/static/media/ketchup-icon.6133a8b7.png"
+  },
+  {
+    "revision": "7dfdb67df4741e4a77aa0dbf34e2ddc7",
+    "url": "/burger-builder/static/media/mayo-icon.7dfdb67d.png"
+  },
+  {
+    "revision": "e6174f3ebc303dcc0368a993ad77b177",
+    "url": "/burger-builder/static/media/mayo.e6174f3e.png"
+  },
+  {
+    "revision": "5664443c16d0038ef9fda7bb9b661224",
+    "url": "/burger-builder/static/media/meat-icon.5664443c.png"
+  },
+  {
+    "revision": "b6014bbb81a3736fd54fbdaa83f28cda",
+    "url": "/burger-builder/static/media/meat.b6014bbb.png"
+  },
+  {
+    "revision": "0893a9cef707a601b7ceaf98c57042f1",
+    "url": "/burger-builder/static/media/mustard-icon.0893a9ce.png"
+  },
+  {
+    "revision": "40e5dea66413e7ba9d9be571db713df4",
+    "url": "/burger-builder/static/media/onion-icon.40e5dea6.png"
+  },
+  {
+    "revision": "b47e68f5fdb5bd2f0a93afe6457aebf8",
+    "url": "/burger-builder/static/media/onion.b47e68f5.png"
+  },
+  {
+    "revision": "57b9f5bc9641a4c4088b73808fc7fcd2",
+    "url": "/burger-builder/static/media/pexels-photo-1108117.57b9f5bc.jpg"
+  },
+  {
+    "revision": "1e86d54f3e130f60a8ed2d7e9fe8cdc1",
+    "url": "/burger-builder/static/media/pickle-icon.1e86d54f.png"
+  },
+  {
+    "revision": "ec82a886f3a473f59ef4d123751e64b2",
+    "url": "/burger-builder/static/media/pickle.ec82a886.png"
+  },
+  {
+    "revision": "e0a460b454990f2120f6dab5df0eece2",
+    "url": "/burger-builder/static/media/spinach.e0a460b4.png"
+  },
+  {
+    "revision": "07edf8ae9249febe610db2d3e2dd5583",
+    "url": "/burger-builder/static/media/tomato-icon.07edf8ae.png"
+  },
+  {
+    "revision": "665889cf8f3af6faeb44cfd79873a840",
+    "url": "/burger-builder/static/media/tomato.665889cf.png"
+  },
+  {
+    "revision": "aa335f0a7ef1f35bee61f6c699f4db15",
+    "url": "/burger-builder/static/media/topBread.aa335f0a.png"
+  }
+]);
